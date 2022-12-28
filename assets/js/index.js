2a8b2aa100ca62f8f8d1c8cc2126c89881b19b8b
bifara <jhon.1701720506@ucaldas.edu.co>
2022-12-28 14:32:16 -0500

    colores full

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,13 +10,15 @@ document.addEventListener('DOMContentLoaded', async () => {
         ground: '#f4e7da',
         rock: '#d5d5d4',
         fairy: '#fceaff',
-        poison: '#98d7a5',
+        poison: '#c00080',
         bug: '#f8d5a3',
         dragon: '#97b3e6',
         psychic: '#eaeda1',
         flying: '#F5F5F5',
         fighting: '#E6E0D4',
-        normal: '#F5F5F5'
+        normal: '#F5F5F5',
+        ghost: '#5b61a1',
+        ice: '#dfe9f5'
     };
     
     // Object.keys: {'fire', 'grass'...}
